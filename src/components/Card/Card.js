34b10dc4feb6bc5142697be1a4965b1f7c8f1329
diogenesblaquier <dblaquier@udesa.edu.ar>
2021-09-15 17:08:08 -0300

    card

---
+++ src/components/Card/Card.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react"
+
+
+class Card extends Component {
+constructor(props){
+    super (props);
+    this.state = {
+        Albumes:[],
+        viewMore: false,
+        text: 'Ver más',
+    }
+}
+
+
+
+};
