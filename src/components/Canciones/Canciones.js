dad16696563eefdd81e72a6fd2c501aa313814f6
diogenesblaquier <dblaquier@udesa.edu.ar>
2021-09-15 01:04:57 -0300

    fetch-canciones.js-header

---
+++ src/components/Canciones/Canciones.js
@@ -0,0 +1,27 @@
+import React, {Component} from "react"
+import Card from '../Card/Card'
+import Header from '../Header/Header'
+
+class Api extends Component {
+constructor(){
+    super ();
+    this.state = {
+        albumes:[],
+        limit: 10,
+    }
+}
+componentDidMount(){
+    let url = ("https://thingproxy.freeboard.io/fetch/https://api.deezer.com/chart/0/albums&top?limit=" + this.state.limit.toString())
+
+    fetch (url)
+    .then(response => response.json())
+    .then((data) => {
+        console.log(data);
+        this.setState({
+            albumes: data.data,
+        })
+    })
+    .catch(error => console.log(error))
+}
+}
+export default Api
